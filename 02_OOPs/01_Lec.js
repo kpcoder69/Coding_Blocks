9b1589b22c246b1f127427b7276d5ddcf1667e5f
Karan pandey <110976645+kpcoder69@users.noreply.github.com>
2025-01-11 19:51:39 +0000

    oops Lec 4

--- 02_OOPs/01_Lec.js
+++ 02_OOPs/01_Lec.js
@@ -89,16 +89,20 @@
 // </head>
 // <body>
 //     <!-- <script>
-//         emp={id:1,name:"Hi there"};
-//         document.write(emp.id+"Name"+emp.name);                    //creating object by object literal (1st way to create object)
-//         var emp=new Object();                                      // in javaScript we directly create Object bcz JS perform template based programming language 
-//         emp.id=1;                                                  // so its not compalsory to create Class in JS
+//         emp={id:1,name:"Hi there"};               //1. creating object by object literal (1st way to create object)
+//         document.write(emp.id+"Name"+emp.name);   // in javaScript we directly create Object bcz JS perform template based programming language 
+//     </script> -->                                  // so its not compalsory to create Class in JS
+//         
+//     <!-- <script>                  
+//         var emp=new Object();                      //2. Creating objects by instance                
+//         emp.id=1;                                                  
 //         emp.name="SJHKKJSNKJn";
 //         document.write(emp.id + "Name" + emp.name);
 //     </script> -->
+//
 //     <!-- <script>
-//        function emp(id,name){
-//         this.id=id;
+//        function emp(id,name){                     //3. creating object by the help of constructor  
+//         this.id=id;                                 //we see there are three different object creation
 //         this.name=name;
 //        }
 //        e=new emp(101,"khabkjckj");
@@ -109,59 +113,147 @@
 // </html>
 
 
+//javascript prototype/template  object 
+//prototype ----> chaining ---->obj ---> Employee ---> obj1 
+/*
+<!DOCTYPE html>
+<html lang="en">
 
-// <!DOCTYPE html>
-// <html lang="en">
-// <head>
-//     <meta charset="UTF-8">
-//     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-//     <title>Document</title>
-// </head>
+<body>
+    <!-- <script>
 
-// <body>
-//     <form name="form1">
-//         Male : <input type="radio" name="gender" value="male"/>
-//         Female : <input type="radio"  name="gender" value="female"/>
-//         <input type="button" onclick="print()" value="Total Gender" />
-//     </form>
-// </body>
-// <script>
-//     function print() {
-//         var allgenders = document.getElementsByName("gender");
-//         alert(allgenders.length);
-//     } 
-// </script>
+        function Employee(firstName,secondName){
+            this.firstName=firstName;
+            this.secondName=secondName;
+        }
+        Employee.prototype.age=12;
+        Employee.prototype.fullName=function(){
+            return this.firstName+this.secondName;
+        }
+        var obj1=new Employee("ABC","DCG");
+        document.writeln(obj1.fullName); 
+        document.writeln(obj1.age); 
+    </script>  -->
+    <script>
 
-// </html> -->
+        class Employee{
+            constructor(){
+               this.age=28;
+            }
+        }
+        class Company extends Employee{
+            constructor(id,name){
+                super();
+                this.id=id;
+                this.name=name;
+            }
+        }
+        var emp=new Company(1,"ABC")
+         document.writeln(emp.id+emp.name+emp.age); 
+    </script>
+</body>
+</html>
+*/
+ 
 
-// <!DOCTYPE html>
-// <html lang="en">
+//Static Method
+/*
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <script>
+        class T = test{
+            constructor(){
+            document.writeln(test.display())
+          
+            }
+            static display(){
+            return "Static method";
+            }
+            static display(){
+            return "Static method 2";
+            }
+            document.writeln(test.display())
+        }
+    </script>
+</body>
+</html> */
 
-// <head>
-//     <meta charset="UTF-8">
-//     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-//     <title>Document</title>
-// </head>
 
-// <body>
+/*
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <script>
+        class test{
+            constructor(){
+               document.writeln(test.display());
+                document.writeln(this.constructor.display());
+           }
 
-//     <br>
+            static display(){
+                return "Hi";
+            }
+            show(){
+                document.writeln(test.display());
+            }
 
-//         Enter no :<input type="text" id="number" name="number"/>
-// </br>
-// <input type="button" value="cube" onclick="print()"/>
+        }
+        var t=new test();
+        t.show();
+    </script>
+</body>
+</html>*/
 
-//     </form>
 
 
-// </body>
-// <script>
-//     function print() {
-//         var no = document.getElementsById("number").value;
-//         alert(no*no*no);
-//     } 
-// </script>
+//Encapsulation
+//read only ; read write ; write
+/*
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+        <script>
+            function Student(name, marks) {
+                var s_name = name;
+                var s_marks = marks;
 
-// </html>
+                Object.defineProperty(this,"name",{
+                    get:function(){
+                        return s_name;
+                    },
+                    set:function(){
+                        this.s_name=s_name;
+                    }
+                });
+                Object.defineProperty(this, "marks", {
+                    get: function () {
+                        return s_marks;
+                    },
+                    set: function () {
+                        this.s_name = s_marks;
+                    }
+                });
+            }
+            var s=new Student("John",54);
+            document.writeln(s.name);
+     </script>
+</body>
+</html> */
