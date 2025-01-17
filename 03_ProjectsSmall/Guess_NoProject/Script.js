58cada0bbd9630d704490561f3abd8134b628385
Karan pandey <110976645+kpcoder69@users.noreply.github.com>
2025-01-17 15:53:55 +0000

    Lec 5

--- 03_ProjectsSmall/Guess_NoProject/Script.js
+++ 03_ProjectsSmall/Guess_NoProject/Script.js
@@ -26,3 +26,12 @@ function disableGame() {
     document.getElementById('guessInput').disabled = true;
     document.querySelector('button[onclick ="check()"]').disabled = true;
 }
+function rest() {
+    no = Math.floor(Math.random() * 100)+1;
+    AttemptLeft = 3;
+    document.getElementById('guessInput').value = ' ';
+    document.getElementById('guessInput').disabled = false;
+    document.querySelector('button[onclick="check()"]').disabled = true;
+    document.getElementById('result').textContent = '';
+    document.getElementById('attempt').textContent = 'AttemptLeft'
+}
